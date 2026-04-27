99543b218d3eadab3b6b11584f3687bbc476ec22
Lenin Ospina <o.lamprea99@gmail.com>
2026-04-26 19:29:12 -0500

    configs de conexion

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //urlApi: 'http://service-backend:7500/api'
-  urlApi: 'http://localhost:7500/api'
+  urlApi: 'http://service-backend:7500/api'
 };
